299f2b81e85a45a7f74d96b521157b2504344e11
Mukunda Johnson <mukunda@mukunda.com>
2025-01-17 01:58:31 -0600

    importing

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#***********************************************************
+# Configure environment
+import venv, os
+
+venv.create(".venv", with_pip=True)
+if os.name == "nt":
+   os.system(".venv\\Scripts\\activate")
+elif os.name == "posix":
+   os.system("source .venv/bin/activate")
+
+os.system("pip install -r requirements.txt")
+#***********************************************************
+
+import os, requests, urllib.request, zipfile
+
+#-----------------------------------------------------------------------------------------
+def run(command):
+   print("Running command: " + command)
+   if os.system(command) != 0:
+      print("Error running command: " + command)
+      exit(1)
+
+#-----------------------------------------------------------------------------------------
+def install_cc65():
+   if os.name == 'nt':
+      # Windows
+      # Download from sourceforge
+      urllib.request.urlretrieve("https://downloads.sourceforge.net/project/cc65/cc65-snapshot-win64.zip", "cc65/cc65-snapshot-win64.zip")
+      with zipfile.ZipFile("cc65/cc65-snapshot-win64.zip", 'r') as zip_ref:
+         zip_ref.extractall("cc65")
+         
+   elif os.name == "posix":
+      # Linux
+      run("git clone https://github.com/cc65/cc65.git")
+      os.chdir("cc65")
+      run("make")
+      os.chdir("..")
+   else:
+      print("install_cc65: Unsupported operating system.")
+
+install_cc65()
